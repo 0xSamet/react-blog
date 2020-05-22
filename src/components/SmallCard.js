82d4397a29b3193807e6d2e1ca2d41d5b956c377
samet <osmansamet58@gmail.com>
2020-05-22 11:11:54 +0300

    posts page card title updated

--- src/components/SmallCard.js
+++ src/components/SmallCard.js
@@ -9,7 +9,7 @@ const SmallCard = (props) => {
     <Card style={{ width: '100%', marginBottom: 10 }}>
        <Card.Img  variant="top" src={image} />
        <Card.Body>
-        <Card.Title>{title}</Card.Title>
+       <Link to={`/post/${_id}`} ><Card.Title>{title}</Card.Title></Link>
         <Card.Text>{content}</Card.Text>
         <Link to={`/post/${_id}`} ><Button variant="primary">Devamını Oku</Button></Link>
        </Card.Body>
